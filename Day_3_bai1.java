a32f4b94da09cd65e7fd55a3a83a197b337a45ca
luyennt18 <79637757+luyennt18@users.noreply.github.com>
2021-03-12 09:16:12 +0700

    Add files via upload

--- Day_3_bai1.java
+++ Day_3_bai1.java
@@ -1,14 +1,19 @@
 package math;
 
+import org.junit.Test;
+
 import java.util.Scanner;
+import java.math.BigDecimal;
+import java.math.BigInteger;
 
 public class Day_3_bai1 {
-
+    @Test
     public static void main(String[] args) {
-        int n = 20;
-        long b = 1;
+        int n = 200;
+
+       BigDecimal b = BigDecimal.ONE;
         for (int i = 1; i <= n; i++) {
-            b = b * i;
+            b = b.multiply(BigDecimal.valueOf(i)) ;
         }
         System.out.println(n + "! = " + b);
 
